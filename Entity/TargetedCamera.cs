45e25b07605cb870e8cf8aa77b5b53b06f7d1b0e
rmtsukuru <randolph.113@osu.edu>
2014-02-21 08:45:09 -0500

    Camera now smoothly follows player.

--- Entity/TargetedCamera.cs
+++ Entity/TargetedCamera.cs
@@ -11,23 +11,20 @@ namespace Nekonigiri
     /// </summary>
     internal class TargetedCamera : Camera
     {
-        private const float Acceleration = .5f;
-
-        private const float MaxVelocity = 4;
 
         /// <summary>
         /// The width, from the center of the screen, to the edge of the camera's focus.
         /// If the target moves out of the focus area, the camera will adjust velocity
         /// in order to track it.
         /// </summary>
-        private const int FocusWidth = 150;
+        private const int FocusWidth = 80;
 
         /// <summary>
         /// The height, from the center of the screen, to the edge of the camera's focus.
         /// If the target moves out of the focus area, the camera will adjust velocity
         /// in order to track it.
         /// </summary>
-        private const int FocusHeight = 100;
+        private const int FocusHeight = 70;
 
         private IGameObject target;
 
@@ -41,38 +38,24 @@ namespace Nekonigiri
             Vector2 position = target.RelativePosition(this);
             if (position.X > OnigiriGame.ScreenCenter.X + FocusWidth)
             {
-                this.Velocity = new Vector2(Math.Min(this.Velocity.X + Acceleration, MaxVelocity),
-                                            this.Velocity.Y);
+                this.Position = new Vector2(target.Position.X - OnigiriGame.WindowWidth / 2 - FocusWidth,
+                                            this.Position.Y);
             }
-            else if (position.X < OnigiriGame.ScreenCenter.Y - FocusWidth)
+            else if (position.X < OnigiriGame.ScreenCenter.X - FocusWidth)
             {
-                this.Velocity = new Vector2(Math.Max(this.Velocity.X - Acceleration, -1 * MaxVelocity),
-                                            this.Velocity.Y);
-            }
-            // If not tracking target, decelerate.
-            else if (Math.Abs(this.Velocity.X) > 0)
-            {
-                float x = (Math.Abs(this.Velocity.X) - Acceleration) * 
-                           this.Velocity.X / Math.Abs(this.Velocity.X);
-                this.Velocity = new Vector2(x, this.Velocity.Y);
+                this.Position = new Vector2(target.Position.X - OnigiriGame.WindowWidth / 2 + FocusWidth,
+                                            this.Position.Y);
             }
 
             if (position.Y > OnigiriGame.ScreenCenter.Y + FocusHeight)
             {
-                this.Velocity = new Vector2(this.Velocity.X,
-                                            Math.Min(this.Velocity.Y + Acceleration, MaxVelocity));
+                this.Position = new Vector2(this.Position.X, target.Position.Y - 
+                                            OnigiriGame.WindowHeight / 2 - FocusHeight);
             }
             else if (position.Y < OnigiriGame.ScreenCenter.Y - FocusHeight)
             {
-                this.Velocity = new Vector2(this.Velocity.X,
-                                            Math.Max(this.Velocity.Y - Acceleration, -1 * MaxVelocity));
-            }
-            // If not tracking target, decelerate.
-            else if (Math.Abs(this.Velocity.Y) > 0) 
-            {
-                float y = (Math.Abs(this.Velocity.Y) - Acceleration) *
-                           this.Velocity.Y / Math.Abs(this.Velocity.Y);
-                this.Velocity = new Vector2(this.Velocity.X, y);
+                this.Position = new Vector2(this.Position.X, target.Position.Y -
+                                            OnigiriGame.WindowHeight / 2 + FocusHeight);
             }
 
             base.Update(gameTime);
@@ -80,25 +63,21 @@ namespace Nekonigiri
             if (this.Position.X < 0)
             {
                 this.Position = new Vector2(0, this.Position.Y);
-                this.Velocity = new Vector2(0, this.Velocity.Y);
             }
             else if (this.Position.X + OnigiriGame.WindowWidth > GameData.Instance.CurrentLevel.Width)
             {
                 float x = GameData.Instance.CurrentLevel.Width - OnigiriGame.WindowWidth;
                 this.Position = new Vector2(x, this.Position.Y);
-                this.Velocity = new Vector2(0, this.Velocity.Y);
             }
 
             if (this.Position.Y < 0)
             {
                 this.Position = new Vector2(this.Position.X, 0);
-                this.Velocity = new Vector2(this.Velocity.X, 0);
             }
             else if (this.Position.Y + OnigiriGame.WindowHeight > GameData.Instance.CurrentLevel.Height)
             {
                 float y = GameData.Instance.CurrentLevel.Height - OnigiriGame.WindowHeight;
                 this.Position = new Vector2(this.Position.X, y);
-                this.Velocity = new Vector2(this.Velocity.X, 0);
             }
         }
     }
